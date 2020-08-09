62aa2aa2727290aaf26a123e34f1606ab517a000
Yuliya Khilko <ykhilko@gmail.com>
2020-08-09 16:26:06 -0700

    feat: add popup handlers

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
 function Main() {
+
+  function handleEditAvatarClick() {
+    document.querySelector(".js-popup-change-avatar").classList.add("popup_role_show");
+  }
+
+  function handleEditProfileClick() {
+    document.querySelector(".js-popup-profile").classList.add("popup_role_show");
+  }
+
+  function handleAddPlaceClick() {
+    document.querySelector(".js-popup-photo-form").classList.add("popup_role_show");
+  }
+
+
   return (
     <main>
 				<section className="profile page__section">
 					<div className="media">
 						<div className="media__image-container">
 							<img alt="profile image" className="media__image" />
-							<button
+							<button onClick={handleEditAvatarClick}
 								className="media__btn media__btn_size_lg media__btn_hoverable"
 								type="button"
 							></button>
@@ -15,7 +29,7 @@ function Main() {
 						<div className="media__body">
 							<div className="media__item">
 								<h1 className="media__name"></h1>
-								<button
+								<button onClick={handleEditProfileClick}
 									className="media__btn media__btn_size_sm button"
 									type="button"
 								></button>
@@ -23,7 +37,7 @@ function Main() {
 							<p className="media__job"></p>
 						</div>
 					</div>
-					<button className="profile__btn button" type="button"></button>
+					<button onClick={handleAddPlaceClick} className="profile__btn button" type="button"></button>
 				</section>
 				<ul className="gallery page__section"></ul>
 			</main>
