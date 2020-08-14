f1bbfc01ab420cdbbae60ac0eabafb4d59153c21
Yuliya Khilko <ykhilko@gmail.com>
2020-08-13 21:59:12 -0700

    make new build

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from '../images/logo.svg';
+import React from "react";
+import logo from "../images/logo.svg";
 
 function Header() {
-  return (
-    <header className="header">
-				<img src={logo} alt="logo" className="header__logo" />
-			</header>
-  );
+	return (
+		<header className="header">
+			<img src={logo} alt="logo" className="header__logo" />
+		</header>
+	);
 }
 
 export default Header;
