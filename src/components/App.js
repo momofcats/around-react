7a652a8b35277955e98a8bceeabafd9d90b583d3
Yuliya Khilko <ykhilko@gmail.com>
2020-08-19 20:30:35 -0700

    feat: add current user context

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import Main from "./Main";
 import Footer from "./Footer";
 import PopupWithForm from "./PopupWithForm";
 import ImagePopup from "./ImagePopup";
+import api from "../utils/Api";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 function App() {
 	const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
@@ -12,6 +14,13 @@ function App() {
 	const [isDelCardPopupOpen, setIsDelCardPopupOpen] = useState(false);
 	const [isImagePopupOpen, setIsImagePopupOpen] = useState(false);
 	const [selectedCard, setSelectedCard] = useState(0);
+	const [currentUser, setCurrentUser] = useState("");
+
+	useEffect(() => {
+		api.getUserInfo().then((data) => {
+			setCurrentUser(data);
+		});
+	}, []);
 
 	function handleCardClick(card) {
 		setSelectedCard(card);
@@ -60,92 +69,94 @@ function App() {
 	return (
 		<>
 			<Header />
-			<Main
-				onEditProfile={handleEditProfileClick}
-				onAddPlace={handleAddPlaceClick}
-				onEditAvatar={handleEditAvatarClick}
-				onDeleteCard={handleDelCardClick}
-				onCardClick={handleCardClick}
-			/>
-			<Footer />
-			<PopupWithForm
-				name="profile"
-				title="Edit profile"
-				buttonText="Save"
-				isOpen={isEditProfilePopupOpen}
-				onClose={closeAllPopups}
-			>
-				<input
-					required
-					pattern="[a-zA-Z\s\-]+"
-					type="text"
-					className="form__input js-input-name"
-					name="name"
-					placeholder="Name"
-					minLength="2"
-					maxLength="40"
+			<CurrentUserContext.Provider value={currentUser}>
+				<Main
+					onEditProfile={handleEditProfileClick}
+					onAddPlace={handleAddPlaceClick}
+					onEditAvatar={handleEditAvatarClick}
+					onDeleteCard={handleDelCardClick}
+					onCardClick={handleCardClick}
 				/>
-				<input
-					required
-					type="text"
-					className="form__input js-input-job"
-					name="about"
-					placeholder="About me"
-					minLength="2"
-					maxLength="200"
+				<Footer />
+				<PopupWithForm
+					name="profile"
+					title="Edit profile"
+					buttonText="Save"
+					isOpen={isEditProfilePopupOpen}
+					onClose={closeAllPopups}
+				>
+					<input
+						required
+						pattern="[a-zA-Z\s\-]+"
+						type="text"
+						className="form__input js-input-name"
+						name="name"
+						placeholder="Name"
+						minLength="2"
+						maxLength="40"
+					/>
+					<input
+						required
+						type="text"
+						className="form__input js-input-job"
+						name="about"
+						placeholder="About me"
+						minLength="2"
+						maxLength="200"
+					/>
+				</PopupWithForm>
+				<PopupWithForm
+					name="photo-form"
+					title="New Place"
+					buttonText="Create"
+					isOpen={isAddPlacePopupOpen}
+					onClose={closeAllPopups}
+				>
+					<input
+						type="text"
+						className="form__input js-input-title"
+						name="name"
+						placeholder="Title"
+						minLength="1"
+						maxLength="30"
+						required
+					/>
+					<input
+						type="url"
+						className="form__input js-input-link"
+						name="link"
+						placeholder="Image link"
+						required
+					/>
+				</PopupWithForm>
+				<PopupWithForm
+					name="change-avatar"
+					title="Change profile picture"
+					buttonText="Save"
+					isOpen={isEditAvatarPopupOpen}
+					onClose={closeAllPopups}
+				>
+					<input
+						type="url"
+						className="form__input js-input-link"
+						name="avatar"
+						placeholder="Url"
+						required
+					/>
+				</PopupWithForm>
+				<PopupWithForm
+					name="del-card"
+					title="Are you sure?"
+					buttonText="Yes"
+					isOpen={isDelCardPopupOpen}
+					onClose={closeAllPopups}
 				/>
-			</PopupWithForm>
-			<PopupWithForm
-				name="photo-form"
-				title="New Place"
-				buttonText="Create"
-				isOpen={isAddPlacePopupOpen}
-				onClose={closeAllPopups}
-			>
-				<input
-					type="text"
-					className="form__input js-input-title"
-					name="name"
-					placeholder="Title"
-					minLength="1"
-					maxLength="30"
-					required
+				<ImagePopup
+					isOpen={isImagePopupOpen}
+					onClose={closeAllPopups}
+					card={selectedCard}
 				/>
-				<input
-					type="url"
-					className="form__input js-input-link"
-					name="link"
-					placeholder="Image link"
-					required
-				/>
-			</PopupWithForm>
-			<PopupWithForm
-				name="change-avatar"
-				title="Change profile picture"
-				buttonText="Save"
-				isOpen={isEditAvatarPopupOpen}
-				onClose={closeAllPopups}
-			>
-				<input
-					type="url"
-					className="form__input js-input-link"
-					name="avatar"
-					placeholder="Url"
-					required
-				/>
-			</PopupWithForm>
-			<PopupWithForm
-				name="del-card"
-				title="Are you sure?"
-				buttonText="Yes"
-				isOpen={isDelCardPopupOpen}
-				onClose={closeAllPopups}
-			/>
-			<ImagePopup
-				isOpen={isImagePopupOpen}
-				onClose={closeAllPopups}
-				card={selectedCard}
-			/>
+			</CurrentUserContext.Provider>
 		</>
 	);
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,32 +1,34 @@
 import React, { useState, useEffect } from "react";
 import api from "../utils/Api";
 import Card from "./Card";
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 function Main(props) {
-	const [userName, setUserName] = useState("");
-	const [userDescription, setUserDescription] = useState("");
-	const [userAvatar, setUserAvatar] = useState("");
-	const [cards, setCards] = useState([]);
+	const currentUser = React.useContext(CurrentUserContext);
+	// const [userName, setUserName] = useState("");
+	// const [userDescription, setUserDescription] = useState("");
+	// const [userAvatar, setUserAvatar] = useState("");
+	// const [cards, setCards] = useState([]);
 
-	useEffect(() => {
-		api.getAppInfo().then(([cards, userData]) => {
-			setUserName(userData.name);
-			setUserDescription(userData.about);
-			setUserAvatar(userData.avatar);
-			cards.map((card) => {
-				card.isOwner = userData._id === card.owner._id;
-				return card;
-			});
-			setCards(cards);
-		});
-	}, []);
+	// useEffect(() => {
+	// 	api.getAppInfo().then(([cards, userData]) => {
+	// 		setUserName(userData.name);
+	// 		setUserDescription(userData.about);
+	// 		setUserAvatar(userData.avatar);
+	// 		cards.map((card) => {
+	// 			card.isOwner = userData._id === card.owner._id;
+	// 			return card;
+	// 		});
+	// 		setCards(cards);
+	// 	});
+	// }, []);
 
 	return (
 		<main>
 			<section className="profile page__section">
 				<div className="media">
 					<div className="media__image-container">
-						<img alt="profile" className="media__image" src={userAvatar} />
+					<img alt="profile" className="media__image" src={currentUser.avatar} />
 						<button
 							onClick={props.onEditAvatar}
 							className="media__btn media__btn_size_lg media__btn_hoverable"
@@ -35,14 +37,14 @@ function Main(props) {
 					</div>
 					<div className="media__body">
 						<div className="media__item">
-							<h1 className="media__name">{userName}</h1>
+							<h1 className="media__name">{currentUser.name}</h1>
 							<button
 								onClick={props.onEditProfile}
 								className="media__btn media__btn_size_sm button"
 								type="button"
 							></button>
 						</div>
-						<p className="media__job">{userDescription}</p>
+						<p className="media__job">{currentUser.about}</p>
 					</div>
 				</div>
 				<button
@@ -52,14 +54,14 @@ function Main(props) {
 				></button>
 			</section>
 			<ul className="gallery page__section">
-				{cards.map((card, id) => (
+				{/* {cards.map((card, id) => (
 					<Card
 						key={id}
 						card={card}
 						onDelete={props.onDeleteCard}
 						onCardClick={props.onCardClick}
 					/>
-				))}
+				))} */}
 			</ul>
 		</main>
 	);
