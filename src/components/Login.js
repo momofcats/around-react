4f1023be30e8845c4222a6b304f829862ec30052
Yuliya Khilko <ykhilko@gmail.com>
2020-10-06 14:03:40 -0700

    styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -34,20 +34,5 @@ function Login(props) {
 		</section>
 	);
 }
-// <section className="authentication page__section">
-//   <div className="authentication__container">
-//     <form>
-//       <h2 className="form__title form__title_theme_light">Log in</h2>
-//       <input className="form__input form__input_theme_light" placeholder="Email" type="email" required />
-//       <input className="form__input form__input_theme_light" placeholder="Password" type="password" required />
-//       <button type="submit" className="form__submit-btn form__submit-btn_theme_light">
-//         Log in
-//       </button>
-//     </form>
-//     <div className="authentication__link">
-//       <span>Not a member yet?</span>
-//       <Link to="#">Sign up here!</Link>
-//     </div>
-//   </div>
-// </section>
+
 export default Login;
